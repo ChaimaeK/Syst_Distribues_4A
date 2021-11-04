bcd072f3da7ef02351efe33a438e5a31eaa622c9
ChaimaeK <90784141+ChaimaeK@users.noreply.github.com>
2021-11-04 23:50:07 +0100

    Create DemoProjet1ApplicationTests.java

---
+++ demoProjet1/src/test/java/fr/polytech/projet/demoprojet1/DemoProjet1ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.projet.demoprojet1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjet1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
